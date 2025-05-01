acc5f9bda1f886af91970fb7c241af175b2f8dd1
riyagarasangi <157198559+riyagarasangi@users.noreply.github.com>
2025-05-02 00:15:57 +0530

    Update app_start.sh

--- backend/app_start.sh
+++ backend/app_start.sh
@@ -2,8 +2,18 @@
 
 cd /home/ec2-user/app
 
+# Install Node.js dependencies
 npm install
 
-# Restart the server
+# Load environment variables from SSM
+export $(aws ssm get-parameters \
+  --names "/we-care/env/NODE_ENV" "/we-care/env/DB_URI" "/we-care/env/API_KEY" \
+  --with-decryption \
+  --query "Parameters[*].{Name:Name,Value:Value}" \
+  --output text | \
+  awk '{split($1,n,"/"); print n[4] "=" $2}'
+)
+
+# Restart the PM2 process
 pm2 stop all || true
 pm2 start server.js --name wellness-app
