deb6c731f53963c85fd8005c27750325fa8fccd1
riyagarasangi <157198559+riyagarasangi@users.noreply.github.com>
2025-05-02 05:36:18 +0530

    Update app_start.sh

--- backend/app_start.sh
+++ backend/app_start.sh
@@ -2,18 +2,25 @@
 
 cd /home/ec2-user/app
 
-# Install Node.js dependencies
 npm install
 
-# Load environment variables from SSM
+# Load environment variables from AWS SSM Parameter Store
 export $(aws ssm get-parameters \
-  --names "/we-care/env/NODE_ENV" "/we-care/env/DB_URI" "/we-care/env/API_KEY" \
+  --names "/we-care/env/PORT" \
+          "/we-care/env/MONGO_URI" \
+          "/we-care/env/FRONTEND_URL_ONE" \
+          "/we-care/env/FRONTEND_URL_TWO" \
+          "/we-care/env/JWT_SECRET_KEY" \
+          "/we-care/env/JWT_EXPIRES" \
+          "/we-care/env/COOKIE_EXPIRE" \
+          "/we-care/env/CLOUDINARY_CLOUD_NAME" \
+          "/we-care/env/CLOUDINARY_API_SECRET" \
+          "/we-care/env/CLOUDINARY_API_KEY" \
   --with-decryption \
   --query "Parameters[*].{Name:Name,Value:Value}" \
-  --output text | \
-  awk '{split($1,n,"/"); print n[4] "=" $2}'
+  --output text | awk '{split($1,n,"/"); print n[4] "=" $2}'
 )
 
-# Restart the PM2 process
+# Restart the server
 pm2 stop all || true
 pm2 start server.js --name wellness-app
